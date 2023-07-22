07688e60d6d49933c04ffdffa2a17a9fc2422256
DerNait <75186178+DerNait@users.noreply.github.com>
2023-07-22 13:17:10 -0600

    Sistema de operaciones al azar
    Agregue ya todas las imágenes de los números y operadores, así como el sistema de que se den las operaciones al azar.

--- Mortal Math 11/Operations.java
+++ Mortal Math 11/Operations.java
@@ -1,44 +1,84 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Random;
 
 public class Operations extends Actor
 {
-    private int timer = 250;
+    private float num1;
+    private int operator;
+    private float num2;
+    private float fResult = 0.0f;
+    private int result = 0;
+    private boolean esEntero = true;
     
-    //Imagenes del Timer.
-    private GreenfootImage imageTimer1 = new GreenfootImage("Timer1.png");
-    private GreenfootImage imageTimer2 = new GreenfootImage("Timer2.png");
-    private GreenfootImage imageTimer3 = new GreenfootImage("Timer3.png");
-    private GreenfootImage imageTimer4 = new GreenfootImage("Timer4.png");
-    private GreenfootImage imageTimer5 = new GreenfootImage("Timer5.png");
-    private GreenfootImage imageTimer6 = new GreenfootImage("Timer6.png");
+    Random random = new Random();
     
     public void act()
     {
-        //Temporizador de 5 segundos
-        switch(timer)
+        
+    }
+    
+    public void asignarValores(){
+        setNum1();
+        setNum2();
+        setOperator();
+    }
+    
+    public void setOperator(){
+        operator = random.nextInt(4)+1;
+    }
+    
+    public int getOperator(){
+        return operator;
+    }
+    
+    public void setNum1(){
+        num1 = random.nextInt(10);
+        System.out.println("Num1: " + num1);
+    }
+    
+    public int getNum1(){
+        int numero1 = (int)num1;
+        return numero1;
+    }
+    
+    public void setNum2(){
+        num2 = random.nextInt(10);
+        System.out.println("Num2: " + num2);
+    }
+    
+    public int getNum2(){
+        int numero2 = (int)num2;
+        return numero2;
+    }
+    
+    public int resultado()
+    {        
+        while(esEntero == true)
         {
-            case 200:
-                setImage(imageTimer2);
-                break;
-            case 150:
-                setImage(imageTimer3);
-                break;
-            case 100:
-                setImage(imageTimer4);
-                break;
-            case 50:
-                setImage(imageTimer5);
-                break;
-            case 0:
-                setImage(imageTimer6);
-                break;
+            setNum1();
+            setNum2();
+            setOperator();
+            
+            switch(operator){
+                case 1:
+                    fResult = num1 + num2;
+                    break;
+                case 2:
+                    fResult = num1 - num2;
+                    break;
+                case 3:
+                    fResult = num1 / num2;
+                    break;
+                case 4:
+                    fResult = num1 * num2;
+                    break;
+            }
+            
+            esEntero = (fResult - Math.floor(fResult) == 0.0f);
         }
         
-        timer--;
+        result = (int)fResult;
         
-        if(timer <= -25){
-            timer = 250;
-            setImage(imageTimer1);
-        }
+        return result;
     }
 }
