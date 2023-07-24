3d3520ca9ff9260c248a8f3dd34dad209173f709
NESHGP04 <89556682+NESHGP04@users.noreply.github.com>
2023-07-23 21:35:16 -0600

    Enemy y player actualizado

--- Mortal Math 11/Enemy.java
+++ Mortal Math 11/Enemy.java
@@ -1,19 +1,114 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
-/**
- * Write a description of class Enemy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Enemy extends Actor
 {
-    /**
-     * Act - do whatever the Enemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int animDuration = 100;
+    private boolean isGettingHit = false;
+    private boolean isAttacking = false;
+    private boolean moveRight = false;
+    private boolean moveLeft = false;
+    private boolean victory = false;
+    private int speed = 20;
+    
     public void act()
     {
-        // Add your action code here.
+        if(victory){
+            winAnimation();
+            return;
+        }
+        
+        if(isGettingHit)
+        damage();
+        
+        if(isAttacking)
+        attack();
+        
+        moveLeftRight();
+    }
+    
+    public void moveLeftRight(){
+        if(getX() <= 1280 && moveRight == true)
+            setLocation(getX()+speed,getY());
+        
+        if(getX() == 1280)
+            setImage("empty.png");
+        
+        if(getX() >= 768 && moveLeft == true)
+            setLocation(getX()-speed,getY());
+    }
+    
+    public void toggleDamage(){
+        isGettingHit = true;
+        System.out.println("Damage");
+    }
+    
+    public void damage(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage hitImage = new GreenfootImage("EHitted.png");
+                this.setImage(hitImage);
+                moveRight = true;
+                break;
+            case 50:
+                moveRight = false;
+                GreenfootImage idleImage = new GreenfootImage("Character2.png");
+                this.setImage(idleImage);
+                moveLeft = true;
+                break;
+        }
+        
+        animDuration--;
+        
+        if(animDuration <= 0){
+            isGettingHit = false;
+            moveLeft = false;
+            animDuration = 100;
+        }        
+    }
+    
+    public void toggleAttack(){
+        isAttacking = true;
+        System.out.println("Attack");
+    }
+    
+    public void attack(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage hitImage = new GreenfootImage("EPunching.png");
+                this.setLocation(681, 496);
+                this.setImage(hitImage);
+                break;
+            case 50:
+                GreenfootImage idleImage = new GreenfootImage("Character2.png");
+                this.setLocation(768, 496);
+                this.setImage(idleImage);
+                break;
+        }
+        
+        animDuration--;
+        
+        if(animDuration <= 0){
+            isAttacking = false;
+            animDuration = 100;
+        } 
+    }
+    
+    public void win(){
+        victory = true;
+    }
+    
+    public void winAnimation(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage punchImage = new GreenfootImage("EPunching.png");
+                setImage(punchImage);
+                break;
+            case 50:
+                GreenfootImage winImage = new GreenfootImage("EVictory.png");
+                setImage(winImage);
+                break;
+        }
+        
+        animDuration--;
     }
 }
--- Mortal Math 11/Player.java
+++ Mortal Math 11/Player.java
@@ -1,19 +1,108 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
-/**
- * Write a description of class Player here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Player extends Actor
 {
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int health = 5;
+    private int animDuration = 100;
+    private boolean isGettingHit = false;
+    private boolean isAttacking = false;
+    private boolean defeated = false;
+    
     public void act()
     {
+        if(defeated){
+            loseAnimation();
+            return;
+        }
         
+        if(isGettingHit)
+        damage();
+        
+        if(isAttacking)
+        attack();
+    }
+    
+    public void toggleDamage(){
+        isGettingHit = true;
+        health--;
+        System.out.println("Damage");
+    }
+    
+    public void damage(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage hitImage = new GreenfootImage("Hitted.png");
+                setImage(hitImage);
+                break;
+            case 50:
+                GreenfootImage recoverImage = new GreenfootImage("Recovering.png");
+                setImage(recoverImage);
+                break;
+        }
+        
+        animDuration--;
+        
+        if(animDuration <= 0){
+            GreenfootImage idleImage = new GreenfootImage("Character1.png");
+            setImage(idleImage);
+            isGettingHit = false;
+            animDuration = 100;
+        }        
+    }
+    
+    public void toggleAttack(){
+        isAttacking = true;
+        System.out.println("Attack");
+    }
+    
+    public void attack(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage punchImage = new GreenfootImage("Punching.png");
+                setLocation(510, 496);
+                setImage(punchImage);
+                break;
+            case 50:
+                GreenfootImage idleImage = new GreenfootImage("Character1.png");
+                setLocation(488, 496);
+                setImage(idleImage);
+                break;
+        }
+        
+        animDuration--;
+        
+        if(animDuration <= 0){
+            isAttacking = false;
+            animDuration = 100;
+        } 
+    }
+    
+    public void lose(){
+        defeated = true;
+        System.out.println("Defeated");
+    }
+    
+    public void loseAnimation(){
+        switch(animDuration){
+            case 100:
+                GreenfootImage hitImage = new GreenfootImage("Hitted.png");
+                setImage(hitImage);
+                break;
+            case 50:
+                GreenfootImage defeatImage = new GreenfootImage("Defeated.png");
+                setImage(defeatImage);
+                break;
+        }
+        
+        animDuration--;
+    }
+    
+    public void setHealth(int health){
+        this.health = health;
+    }
+    
+    public int getHealth(){
+        return health;
     }
 }
