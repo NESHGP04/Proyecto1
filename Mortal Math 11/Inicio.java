a3c82b0b3939311234b0e6da8a234a003c665db3
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2023-07-31 10:52:42 -0600

    Add files via upload

--- Mortal Math 11/Inicio.java
+++ Mortal Math 11/Inicio.java
@@ -9,13 +9,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Inicio extends World
 {
     private Button playButton = new Button("bee.png",new MyWorld());
-    private Button coopButton = new Button("img.png",new MyWorldCoop());
-    private Button versusButton= new Button("salir.png",new MyWorldVersus());
+    private Button coopButton = new Button("salir.png",new MyWorldCoop());
+    private Button versusButton= new Button("vs.png",new MyWorldVersus());
     public Inicio()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1);
         addObject(playButton,150,100);
         addObject(versusButton,150,500);
+        addObject(coopButton,300,500);
     }
 }
